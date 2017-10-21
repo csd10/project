d5fc9e11d81eea85a317efa42d0b98d0726acd29
lakshana123 <32977954+lakshana123@users.noreply.github.com>
2017-10-21 15:10:19 +0530

    Oop

---
+++ Oop/src/edu/bcas/oc/Puppy.java
@@ -0,0 +1,11 @@
+package edu.bcas.oc;
+
+public class Puppy {
+	
+	public Puppy() {//Puppy is the counstractor
+		System.out.println("Hi default Puppy");
+	}
+	public Puppy(String name) {//Puppy is the counstractor with parameter
+
+	}
+}
